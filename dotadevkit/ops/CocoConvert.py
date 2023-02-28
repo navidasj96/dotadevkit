998d8a28d2abadf7f275ab41a6f7a2eacf62c2ef
navidasj96 <89703564+navidasj96@users.noreply.github.com>
2023-02-28 14:48:59 +0330

    Update CocoConvert.py

--- dotadevkit/ops/CocoConvert.py
+++ dotadevkit/ops/CocoConvert.py
@@ -1,88 +1,68 @@
-# --------------------------------------------------------
-# Modified by Ashwin Nair
-# Written by Jian Ding for DOTA_Devkit
-# --------------------------------------------------------
-
+import dota_utils as util
+import os
 import cv2
 import json
 
-from dotadevkit.misc.dota_utils import dota_classes, parse_dota_poly2
-from pathlib import Path
+wordname_15 = ['plane', 'baseball-diamond', 'bridge', 'ground-track-field', 'small-vehicle', 'large-vehicle', 'ship', 'tennis-court',
+               'basketball-court', 'storage-tank',  'soccer-ball-field', 'roundabout', 'harbor', 'swimming-pool', 'helicopter']
 
-
-def DOTA2COCO(srcpath, destfile, version="1.0"):
-    imageparent = srcpath / "images"
-    labelparent = srcpath / "labelTxt"
-    assert version in ["1.0", "1.5", "2.0"]
-
-    if version == "1.5":
-        dota_classes.append("container-crane")
-
-    if version == "2.0":
-        dota_classes.extend(["container-crane", "airport", "helipad"])
+def DOTA2COCO(srcpath, destfile):
+    imageparent = os.path.join(srcpath, 'images')
+    labelparent = os.path.join(srcpath, 'labelTxt')
 
     data_dict = {}
-    info = {
-        "contributor": "Captain Group, Wuhan University",
-        "data_created": "2018",
-        "description": f"DOTA dataset version {version}",
-        "url": "https://captain-whu.github.io/DOTA/dataset.html",
-        "version": version,
-        "year": 2018,
-    }
-    data_dict["info"] = info
-    data_dict["images"] = []
-    data_dict["categories"] = []
-    data_dict["annotations"] = []
-
-    for idex, name in enumerate(dota_classes):
-        single_cat = {"id": idex + 1, "name": name, "supercategory": name}
-        data_dict["categories"].append(single_cat)
+    info = {'contributor': 'captain group',
+           'data_created': '2018',
+           'description': 'This is 1.0 version of DOTA dataset.',
+           'url': 'http://captain.whu.edu.cn/DOTAweb/',
+           'version': '1.0',
+           'year': 2018}
+    data_dict['info'] = info
+    data_dict['images'] = []
+    data_dict['categories'] = []
+    data_dict['annotations'] = []
+    for idex, name in enumerate(wordname_15):
+        single_cat = {'id': idex + 1, 'name': name, 'supercategory': name}
+        data_dict['categories'].append(single_cat)
 
     inst_count = 1
     image_id = 1
-    with open(destfile, "w") as f_out:
-        filenames = [lbl for lbl in labelparent.iterdir()]
+    with open(destfile, 'w') as f_out:
+        filenames = util.GetFileFromThisRootDir(labelparent)
         for file in filenames:
-            basename = file.stem
+            basename = util.custombasename(file)
+            # image_id = int(basename[1:])
 
-            imagepath = imageparent / (basename + ".png")
-            img = cv2.imread(str(imagepath))
+            imagepath = os.path.join(imageparent, basename + '.png')
+            img = cv2.imread(imagepath)
             height, width, c = img.shape
 
             single_image = {}
-            single_image["file_name"] = basename + ".png"
-            single_image["id"] = image_id
-            single_image["width"] = width
-            single_image["height"] = height
-            data_dict["images"].append(single_image)
+            single_image['file_name'] = basename + '.png'
+            single_image['id'] = image_id
+            single_image['width'] = width
+            single_image['height'] = height
+            data_dict['images'].append(single_image)
 
             # annotations
-            objects = parse_dota_poly2(file)
+            objects = util.parse_dota_poly2(file)
             for obj in objects:
                 single_obj = {}
-                single_obj["area"] = obj["area"]
-                single_obj["category_id"] = dota_classes.index(obj["name"]) + 1
-                single_obj["segmentation"] = []
-                single_obj["segmentation"].append(obj["poly"])
-                single_obj["iscrowd"] = 0
-                xmin, ymin, xmax, ymax = (
-                    min(obj["poly"][0::2]),
-                    min(obj["poly"][1::2]),
-                    max(obj["poly"][0::2]),
-                    max(obj["poly"][1::2]),
-                )
+                single_obj['area'] = obj['area']
+                single_obj['category_id'] = wordname_15.index(obj['name']) + 1
+                single_obj['segmentation'] = []
+                single_obj['segmentation'].append(obj['poly'])
+                single_obj['iscrowd'] = 0
+                xmin, ymin, xmax, ymax = min(obj['poly'][0::2]), min(obj['poly'][1::2]), \
+                                         max(obj['poly'][0::2]), max(obj['poly'][1::2])
 
                 width, height = xmax - xmin, ymax - ymin
-                single_obj["bbox"] = xmin, ymin, width, height
-                single_obj["image_id"] = image_id
-                data_dict["annotations"].append(single_obj)
-                single_obj["id"] = inst_count
+                single_obj['bbox'] = xmin, ymin, width, height
+                single_obj['image_id'] = image_id
+                data_dict['annotations'].append(single_obj)
+                single_obj['id'] = inst_count
                 inst_count = inst_count + 1
             image_id = image_id + 1
         json.dump(data_dict, f_out)
-
-
-if __name__ == "__main__":
-    out_dir = Path("/home/ashwin/Desktop/Projects/dotadevkit/example_split")
-    DOTA2COCO(out_dir, out_dir / "DOTA_example.json", version="1.0")
+if __name__ == '__main__':
+    DOTA2COCO(r'/data0/data_dj/1024_new', r'/data0/data_dj/1024_new/DOTA_trainval1024.json')
